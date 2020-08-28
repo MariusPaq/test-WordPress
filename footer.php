7d5d63065b6b58f926eb5e499e5d939306ccdc8a
MariusPaq <marius.paquet@gmail.com>
2020-08-28 16:45:27 +0200

    menuFooter

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
-  </div><!-- FIN container-->
+    <footer id="footer">
+      <div class="row">
+        <div class="col">
+          <h5>ARCHIVES</h5>
+        </div>
+        <div class="col">
+          <h5>GATEGORIES</h5>
+          <?php the_category();?>
+        </div>
+        <div class="col">
+          <h5>META</h5>
+        </div>
+      </div>
+    </footer>
   <?php wp_footer(); ?>
+  </div><!-- FIN container-->
   </body>
 </html>
