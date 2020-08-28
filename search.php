230cadae7563a5b20b81df7f96dd1b6eb8c00b47
MariusPaq <marius.paquet@gmail.com>
2020-08-28 16:44:18 +0200

    .

--- search.php
+++ search.php
@@ -10,25 +10,21 @@ global $wp_query;
 
         <?php if ( have_posts() ) { ?>
 
-            <ul>
-
             <?php while ( have_posts() ) { the_post(); ?>
 
-               <li>
-                 <h3><a href="<?php echo get_permalink(); ?>">
-                   <?php the_title();  ?>
-                 </a></h3>
-                 <?php  the_post_thumbnail('medium') ?>
-                 <?php echo substr(get_the_excerpt(), 0,200); ?>
-                 <div class="h-readmore"> <a href="<?php the_permalink(); ?>">Read More</a></div>
-               </li>
+              <div class="resultsFind">
+                <h3><?php the_title();  ?></h3>
+                <?php  the_post_thumbnail('medium') ?>
+                <?php echo substr(get_the_excerpt(), 0,200); ?>
+                <div class="h-readmore"> <a href="<?php the_permalink(); ?>">Lire la suite</a></div>
+              </div>
 
             <?php } ?>
-
-            </ul>
 
            <?php echo paginate_links(); ?>
 
         <?php } ?>
 
     </div>
+
+<?php include 'footer.php' ?>
